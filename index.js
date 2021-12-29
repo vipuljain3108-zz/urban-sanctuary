12d8ad1caf5aa72b144e847f9efd307551f9e7e1
Vipul Jain <vipul.jain@neosoftmail.com>
2021-12-29 13:49:50 +0530

    updated build

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 
 /*
   ===========================================================================
-            Setup express + passportjs server for authentication
+            Setup express 
   ===========================================================================
 */
 
