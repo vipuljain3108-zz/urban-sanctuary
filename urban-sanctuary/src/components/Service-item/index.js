662ad533b62f6f1dae4ac8e578bfd5df7e990809
Karan Verma <karan.verma@labyrinth.ai>
2022-01-09 19:38:48 +0530

    Added flip to services

--- urban-sanctuary/src/components/Service-item/index.js
+++ urban-sanctuary/src/components/Service-item/index.js
@@ -3,8 +3,13 @@ import './index.scss';
 
 const Index = ({ cn = '', item, uppercase = false }) => {
     return (
-        <div className={`service_item ${cn}`} style={{ backgroundImage: `url(${item.image})`}}>
-            <p style={{ textTransform: `${uppercase ? 'uppercase' : ''}`}}>{item?.name}</p>
+        <div className={`card`}>
+            <div className={`service_item_front ${cn}`} style={{ backgroundImage: `url(${item.image})`}}>
+                <p style={{ textTransform: `${uppercase ? 'uppercase' : ''}`}}>{item?.name}</p>
+            </div>  
+            <div className={`service_item_back ${cn}`}>
+                <p style={{ textTransform: `${uppercase ? 'uppercase' : ''}`}}>{item?.name}</p>
+            </div> 
         </div>
     )
 }
