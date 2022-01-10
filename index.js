46c2a6d314220be7f6de10ac8306497de0b9bc1e
Vipul Jain <vipul.jain@neosoftmail.com>
2022-01-10 11:16:10 +0530

    build updated

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ var express = require('express');
 var app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname,'/urban-sanctuary', '/build')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/build/index.html'));
+    res.sendFile(path.join(__dirname, '/urban-sanctuary', '/build/index.html'));
 })
 
 app.listen(port, () => {
