504ed0df4e8ecdbab1185eb4cd78da6478db3412
Vipul Jain <vipul.jain@neosoftmail.com>
2022-01-18 21:41:10 +0530

    build

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ app.get('/*', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`app listening at http://localhost:${port}`)
+});
